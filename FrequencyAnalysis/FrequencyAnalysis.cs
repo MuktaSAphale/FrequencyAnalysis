d87b9b9c94b71240e6de54baca5be45648bcffce
MuktaSAphale <mukta_aphale@yahoo.com>
2025-05-05 14:39:04 +0100

    Seperated main program and the class

---
+++ FrequencyAnalysis/FrequencyAnalysis.cs
@@ -0,0 +1,64 @@
+﻿/* SWORD Programming Test 
+ * Mukta S. Aphale */
+
+using System.Collections;
+
+namespace FrequencyAnalysis
+{
+    class FrequencyAnalysis
+    {
+        private List<char> excludeCharsList = new List<char> { ' ', '\n', '\t', '\r' };
+        private string text { get; set; }
+
+        private int charCountDirectory = 0;
+
+        private int charCountLinq = 0;
+
+        public FrequencyAnalysis(string text)
+        {
+            this.text = text;
+        }
+
+        public int GetCharCountDirectory()
+        {
+            return charCountDirectory;
+        }
+
+        public int getCharCountLinq() 
+        {
+            return charCountLinq;
+                
+        }
+
+        public Dictionary<char, int> DirectorySolution()
+        {
+            /* Solution without using LINQ */           
+             var charFrequency = new Dictionary<char, int>();
+            foreach (var c in text)
+            {
+                if (!excludeCharsList.Contains(c))
+                {
+                    if (charFrequency.ContainsKey(c))
+                        charFrequency[c]++;
+                    else
+                        charFrequency[c] = 1;
+                    this.charCountDirectory++;
+                }
+            }
+            return charFrequency;
+        }
+
+        public Dictionary<char, int> LinqSolution()
+        {
+             /* Solution using LINQ */
+            charCountLinq = text.Select(x => x).Where(x => !excludeCharsList.Contains(x)).Count();
+
+            Dictionary<char, int> alphabetsCount = text
+                                .GroupBy(x => x)
+                                .Where(x => !excludeCharsList.Contains(x.Key))
+                                .ToDictionary(x => x.Key, x => x.Count());
+
+            return alphabetsCount;
+        }
+    }   
+}
---
+++ FrequencyAnalysis/MainProgram.cs
@@ -0,0 +1,55 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace FrequencyAnalysis
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            string text = "";
+
+            if (args.Count() > 0)
+            {
+                try
+                {
+                    text = System.IO.File.ReadAllText(args[0]);
+                }
+                catch (System.IO.FileNotFoundException)
+                {
+                    Console.WriteLine($"File {args[0]} not found");
+                    return;
+                }
+
+                if ((args.Count() == 2) && (args[1] != null) && (args[1].Equals("ignorecase")))
+                    text = text.ToUpper();
+
+                FrequencyAnalysis analyse = new FrequencyAnalysis(text);
+
+                 Console.WriteLine("Solution using Dictonary");
+                var charFrequency = analyse.DirectorySolution();
+
+                Console.WriteLine("Total Number of Charachters " + analyse.GetCharCountDirectory());
+                foreach (KeyValuePair<char, int> keyValuePair in charFrequency.OrderByDescending(key => key.Value).Take(10))
+                {
+                    Console.WriteLine("{0} ({1})", keyValuePair.Key, keyValuePair.Value);
+                }
+
+                Console.WriteLine("===========================");
+                Console.WriteLine("Solution using LINQ");
+                
+                var alphabetsCount = analyse.LinqSolution();
+
+                Console.WriteLine("Total Number of Charachters " + analyse.getCharCountLinq());
+                foreach (KeyValuePair<char, int> keyValuePair in alphabetsCount.OrderByDescending(key => key.Value).Take(10))
+                {
+                    Console.WriteLine("{0} ({1})", keyValuePair.Key, keyValuePair.Value);
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("File name expected as an argument");
+            }
+        }
+    }
+}
